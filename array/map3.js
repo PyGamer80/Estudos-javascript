7ceb3f54fc8e6a4da0d0fac47b94d7a8fc4d1475
Pygamer80 <pygamer80@gmail.com>
2022-05-13 22:41:40 -0300

    Atualização

---
+++ array/map3.js
@@ -0,0 +1,23 @@
+Array.prototype.map2 = function(callback){
+    const newArray = []
+    for (let i = 0; i < this.length; i ++){
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
+
+const carrinho = [
+    '{"nome": "Borracha", "preco":3.45 }',
+    '{"nome":"caderno", "preco": 13.90}',
+    '{"nome":"kit de laps", "preco": 41.22}',
+    '{"nome":"caneta", "preco":7.50}'
+]
+
+//Retorne um array com os preços
+
+const paraObjeto = json => JSON.parse(json)
+
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log(resultado)
